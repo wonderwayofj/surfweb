a459f7b35fdc9017c8158979af5c8fef98f83664
wonderwayofj <105504066+wonderwayofj@users.noreply.github.com>
2026-03-13 16:44:46 +0100

    Update en.js

--- resources/en.js
+++ resources/en.js
@@ -13,7 +13,7 @@ var TRANSLATIONS_EN = {
   "who_video_cta": "→ watch the teaser",
 
   "why_title": "my journey",
-  "why_text": "🏄 Surfing is my path of discovery. I push my limits ✨, explore the world 🌍 and share this journey with others 🧘. Because I believe adventure can inspire people to pursue their dreams. 🌊"
+  "why_text": "🏄 surfing is my path of discovery. i push my limits ✨, explore the world 🌍 and share this journey with others 🧘. because i believe adventure can inspire people to pursue their dreams. 🌊"
   
   "results_title": "results",
   "result_1": "🥈 2nd place Berber Cup 2026 Morocco CZ/SK",
